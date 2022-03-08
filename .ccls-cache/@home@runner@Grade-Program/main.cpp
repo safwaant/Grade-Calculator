2a9024ced66efd1097e8ddfd3ea9adcebcc05384
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-03-08 00:14:18 +0000

    Finish main

--- .ccls-cache/@home@runner@Grade-Program/main.cpp
+++ .ccls-cache/@home@runner@Grade-Program/main.cpp
@@ -1,5 +1,5 @@
 #include <iostream>
-#include <fstream>;
+#include <sstream>
 using namespace std;
 
 static const double HW_WEIGHT = 0.35;
@@ -10,14 +10,21 @@ int main() {
   cout << "Enter Your Assignment: ";
   double assignTotal(0.0);
   int nums(0);  
-  string temp("");  
-  while(getline(cin, temp)){
+  string temp(""); 
+  getline(cin, temp);
+  stringstream str(temp);   
+  while(str >> temp){
     assignTotal += stod(temp); 
     ++nums;  
-  }
-  double assignAvg(assignTotal / (30 * nums));
+  } 
+  double hwAvg((assignTotal / (30 * nums)) * 100);
+  cerr << hwAvg << endl;  
   cout << "Enter your Midterm Score: ";
   double midtermScore(0.0);  
   cin >> midtermScore;
-  cout <<   
+  cout << "Enter your Final score: ";
+  double finalScore(0.0);
+  cin >> finalScore;
+  double weightedAvg = ( (HW_WEIGHT * hwAvg) + (MIDTERM_WEIGHT * midtermScore) + (FINAL_WEIGHT * finalScore) ) / (HW_WEIGHT + MIDTERM_WEIGHT + FINAL_WEIGHT);  
+  cout << "Your course avg: " << weightedAvg;  
 }
