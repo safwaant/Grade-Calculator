9395ee925fb5a3c9057a00e718afa92c97b9c4ab
Safwaan Taher <62581576+safwaant@users.noreply.github.com>
2022-03-09 01:21:24 -0800

    Add comments

--- main.cpp
+++ main.cpp
@@ -1,17 +1,21 @@
 #include <iostream>
 #include <sstream>
+#include <cmath>
 using namespace std;
 
 static const double HW_WEIGHT = 0.35;
 static const double MIDTERM_WEIGHT = 0.30;
 static const double FINAL_WEIGHT = 0.35;
+static const double HW_PTS = 30.0;
 
 int main() {
-  cout << "Enter Your Assignment: ";
+  // starting line inputs assignments token by token
+  cout << "Enter Your Assignments Scores (Each seperated by a space): ";
 
-  double assignTotal(0.0);
-  int nums(0);  
-  string temp(""); 
+  // declaring varaibles to store assignment data
+  double assignTotal(0.0); int nums(0); string temp(""); 
+  
+  // read all assignments inputted from user
   getline(cin, temp);
   stringstream str(temp);   
   while(str >> temp){
@@ -19,22 +23,46 @@ int main() {
     ++nums;  
   } 
 
-  double hwAvg((assignTotal / (30 * nums)) * 100);
+  // find average score for homework
+  double hwAvg((assignTotal / (HW_PTS * nums)) * 100);
+
+  // get and store midterm score
   cout << "Enter your Midterm Score: ";
   double midtermScore(0.0);  
   cin >> midtermScore;
-  cout << "Enter your Final score: ";
+
+  // get and store final score
+  cout << "Enter your Final score (Or expected score): ";
   double finalScore(0.0);
   cin >> finalScore;
+
+  // calculate final weighted average
   double weightedAvg = ((HW_WEIGHT * hwAvg)
     + (MIDTERM_WEIGHT * midtermScore) 
     + (FINAL_WEIGHT * finalScore)) 
     / (HW_WEIGHT + MIDTERM_WEIGHT + FINAL_WEIGHT); 
 
-  cout << "-----------------------------\n" 
+  // output result
+  cout << "\n-----------------------------\n" 
   << "Your course avg: " 
   << weightedAvg 
   << endl 
   << "-----------------------------" 
   << endl;  
+
+  int roundedAvg = static_cast<int>(round(weightedAvg));
+  /*
+  double gpa(0.0);
+  switch(roundedAvg){
+    case(99):
+      gpa = 4.0;
+      break;
+    case(9):
+      gpa = 4.0;
+      break;
+    case(99):
+      gpa = 4.0;
+      break;  
+  }
+  */
 }
